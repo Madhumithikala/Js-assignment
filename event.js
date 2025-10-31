7c82ac9dbe5c1e035d34a14d0c917267cade1e04
Mithikala Madhu <madhumithikala60@gmail.com>
2025-10-31 11:41:16 +0530

    JS Assign

---
+++ event.js
@@ -0,0 +1,32 @@
+// onmouseover
+function abc() {
+    document.getElementById('a').style.backgroundColor = 'green';
+}
+
+// onmouseout
+function bca() {
+    document.getElementById('b').style.backgroundColor = 'red';
+}
+
+// onclick
+function cab() {
+    document.getElementById('c').style.backgroundColor = 'yellow';
+}
+
+// ondblclick
+function xyz() {
+    document.getElementById('x').style.backgroundColor = 'blue';
+}
+
+// onfocus
+function Guru() {
+    document.getElementById('y').style.backgroundColor = 'pink';
+}
+
+// onblur
+function maa() {
+    let input = document.getElementById("GF");
+    input.value = input.value.toUpperCase();
+    //input.value = input.value.toLowerCase();
+}
+
